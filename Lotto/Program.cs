7579884cf1194cdc75477017c44db0879d505f7d
Bringfireh <ngutortsenongo@gmail.com>
2022-04-22 23:08:39 +0100

    modified program cs

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -6,7 +6,25 @@ namespace Lotto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+                Random random = new Random();
+                int[] arrList = new int[50];
+
+                for (int i = 1; i <= 50; i++)
+                {
+                    for (int j = 1; j < 7; j++)
+                    {
+                        //int iNum = random.Next(1, 50);
+
+                        arrList[j] = random.Next(1, 50);
+                        //Console.WriteLine(random.Next(1, 50).ToString());
+                        Console.WriteLine(random.Next(1, 50).ToString());
+
+
+                }
+                    Console.WriteLine("-----------------");
+                }
+           
         }
     }
 }
