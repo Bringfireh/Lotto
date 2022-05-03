65e505ed6f7a59107063bd632f33a5b3dd278689
Bringfireh <ngutortsenongo@gmail.com>
2022-05-03 17:07:42 +0100

    completed the random number generation

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace Lotto
 {
     class Program
@@ -8,22 +9,29 @@ namespace Lotto
         {
            
                 Random random = new Random();
-                int[] arrList = new int[50];
+                List<int> arrList = new List<int>();
 
                 for (int i = 1; i <= 50; i++)
                 {
-                    for (int j = 1; j < 7; j++)
-                    {
-                        //int iNum = random.Next(1, 50);
+                        List<int> tempnum = new List<int>();
+                        for (int j = 1; j < 7; j++)
+                            {
+                    
+                                int randnum = random.Next(1,50);
+                                //instead of tempnum, you can use arrList in the while for the numbers not to repeat.
+                                while (tempnum.Where(x => x.Equals(randnum)).Count()>0)
+                                {
+                                    randnum = random.Next(1,50);
+                                }
+                                tempnum.Add(randnum);
+                                arrList.Add(randnum);
+                                Console.WriteLine(randnum);
 
-                        arrList[j] = random.Next(1, 50);
-                        //Console.WriteLine(random.Next(1, 50).ToString());
-                        Console.WriteLine(random.Next(1, 50).ToString());
 
-
-                }
+                        }
                     Console.WriteLine("-----------------");
-                }
+                    Console.WriteLine("-----------------");
+            }
            
         }
     }
